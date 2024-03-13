761257177e9a01e3e44d39b59903cddb1c9251b6
Ana <asakalieu@gmail.com>
2024-03-13 13:37:37 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+// function tarea(callback) {
+//     console.log('mi primera tarea');
+//     callback(); //Hace que se finalice la tarea
+// }
+
+// // NODE         JS
+// exports.tarea = tarea;
+
+//Extraer la funcionalidad de gulp desde la carpeta contenida en node-moduler. Como si fuera un import en Java
+const { src, dest, watch } = require("gulp"); //Importo el modulo de gulp para trabajar con sus funciones y metodos
+//src: Es una funcion que permite indicar los archivos fuente a utilizar en nuestro proyecto
+//dest: Es una funcion que nos permite  indicar donde queremos enviar o guardar estos archivos fuente (carpeta destino).
+//watch para que el correr el script, este ejecutando los cambios en live
+
+// const sass = require("sass"); //Cargamos el plugin SASS para poder compilarlo a CSS (import de sass). Pero no sirve, no conecta gulp con sass
+const sass = require("gulp-sass")(require('sass')); //Cargamos el plugin SASS de GULP
+
+function css(done) {
+  src("src/scss/app.scss")    //identifica el archivo sass
+    .pipe(sass())             //aplica sass. compila
+    .pipe(dest("build/css")); //guarda en css. almacena en disco
+    done();
+}
+
+function dev(done) {
+    watch("src/scss/app.scss", css)
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
